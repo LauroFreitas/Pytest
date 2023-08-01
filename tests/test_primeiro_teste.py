2c57e37afd79d1eec4deeb579b24aeb12e7e6d50
laurofreitas <lauroh.sfreitas@gmail.com>
2023-08-01 11:46:01 -0300

    Adição de novos testes e novas funcionalidades na classe Product

--- tests/test_primeiro_teste.py
+++ tests/test_primeiro_teste.py
@@ -11,28 +11,67 @@ def novoProduto_Fixture():
 @mark.parametrize(
     'nome, price, stock_quantity',
     [
-        ('Teste', 100, 2),
-        ('Teste2', 200, 5),
-        ('Teste3', 0, 10),
+        # Arrange
+        ('Calça', 150, 5),('Sapato', 300, 3),('Blusa', 78, 20),
+
     ]
 )
 def test_adiciona_três_novos_produtos_valida_se_os_produtos_foram_adicionados_corretamente(nome, price, stock_quantity):
+    # Act
     novoProduto = Product(nome, price, stock_quantity)
+   
+    # Assert
     assert novoProduto.name == nome
     assert novoProduto.price == price
     assert novoProduto.stock_quantity == stock_quantity
     
   
-
 def test_cenário_de_adição_de_produtos_ao_estoque_incluir_um_valor_inicial_e_modificar_para_3_e_validar_mensagem(novoProduto_Fixture):
-  assert novoProduto_Fixture.update_stock(3) == (f"A quantidade em estoque do produto '{novoProduto_Fixture.name}' foi atualizada para {novoProduto_Fixture.stock_quantity} unidades.")
-  assert novoProduto_Fixture.stock_quantity == 3
-
-
+    # Arrange
+    resultado = novoProduto_Fixture.update_stock(3)
+    
+    # Act
+    mensagem_esperada = f"A quantidade em estoque do produto '{novoProduto_Fixture.name}' foi atualizada para {novoProduto_Fixture.stock_quantity} unidades."
+    
+    # Assert
+    assert mensagem_esperada in resultado
+    assert novoProduto_Fixture.stock_quantity == 3
 
 def test_cenário_de_adição_inválida_de_produtos_ao_estoque_incluir_um_valor_inicial_e_modificar_para_um_valor_invalido(novoProduto_Fixture):
-    assert novoProduto_Fixture.update_stock(-1) == (f"O estoque do produto '{novoProduto_Fixture.name}' está negativo. Verifique o estoque!")
+    # Arrange
+    resultado = novoProduto_Fixture.update_stock(-1)
+    
+    # Act
+    mensagem_esperada = f"O estoque do produto '{novoProduto_Fixture.name}' está negativo. Verifique o estoque!"
+    
+    # Assert
+    assert mensagem_esperada in resultado
     assert novoProduto_Fixture.stock_quantity == 1
 
-def test_verifica_se_o_produto_está_em_promoção_com_base_no_preço_fornecido():
-   ...
+def test_verifica_promoção_para_produto_com_oferta(novoProduto_Fixture):
+   # Arrange
+    novoProduto_Fixture.apply_discount(10)
+    novaOferta = novoProduto_Fixture.price
+
+    # Act
+    novoProduto_Fixture.price = novaOferta
+
+    # Assert
+    assert novaOferta == 90, f"O preço com desconto ficou {novaOferta}"
+
+
+@mark.parametrize(
+    'stock_quantity, valor_esperado_no_estoque',
+    [
+        (4, 400),  
+        (6, 600),   
+        (8, 800),   
+    ]
+)
+def test_verifica_valor_total_no_estoque_para_produto_cadastrado(stock_quantity, valor_esperado_no_estoque, novoProduto_Fixture):
+    # Act 
+    novoProduto_Fixture.stock_quantity = stock_quantity
+    valorTotalEstoque = novoProduto_Fixture.get_total_value()
+
+    # Assert
+    assert valorTotalEstoque == valor_esperado_no_estoque
